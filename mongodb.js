954516f39e029707eb650809116496056d6203de
levvw <abdelraahmanmostafa@gmail.com>
2021-10-29 21:36:43 +0200

    made all CRUD operatings in mongodb

--- mongodb.js
+++ mongodb.js
@@ -1,7 +1,7 @@
 // CRUD - create read update delete
 const mongodb = require("mongodb"); // npm i mongodb
 // const MongoClient = mongodb.MongoClient;
-const MongoClient = mongodb.MongoClient;
+const { MongoClient, ObjectId } = mongodb;
 const connectionURL = "mongodb://127.0.0.1:27017";
 const databaseName = "task-manager"; // or any name project related
 
@@ -17,42 +17,11 @@ MongoClient.connect(
     console.log("Connected to database");
 
     const db = client.db(databaseName);
-
-    db.collection("users").insertOne(
-      {
-        name: "Levw",
-        age: 20,
-      },
-      (error, result) => {
-        if (error) {
-          console.log("Unable to insert user");
-          return;
-        }
-        console.log(result);
-      }
-    );
-    db.collection("new-tasks").insertMany(
-      [
-        {
-          description: "study the course",
-          completed: true,
-        },
-        {
-          description: "go the gym",
-          completed: true,
-        },
-        {
-          description: "read books",
-          completed: false,
-        },
-      ],
-      (error, result) => {
-        if (error) {
-          console.log("Unable to insert tasks");
-          return;
-        }
-        console.log(result);
-      }
-    );
-  } // view it from robo 3t
+    db.collection("users").deleteOne({ name: "Levw" }, (error, result) => {
+      console.log(result);
+    });
+    db.collection("users").deleteMany({ age: 20 }, (error, result) => {
+      console.log(result);
+    });
+  }
 );
