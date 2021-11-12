7524f6c94c6d342486cc2cc5905832235e805ece
levvw <abdelraahmanmostafa@gmail.com>
2021-11-12 12:13:20 +0200

    created route for logging in users

--- src/routers/user.js
+++ src/routers/user.js
@@ -1,80 +1,99 @@
-const express = require('express')
-const User = require('../models/user')
-const router = new express.Router()
+const express = require("express");
+const User = require("../models/user");
+const router = new express.Router();
+const bcrypt = require("bcryptjs");
+router.post("/users", async (req, res) => {
+  const user = new User(req.body);
 
-router.post('/users', async (req, res) => {
-    const user = new User(req.body)
+  try {
+    await user.save();
+    res.status(201).send(user);
+  } catch (e) {
+    res.status(400).send(e);
+  }
+});
 
-    try {
-        await user.save()
-        res.status(201).send(user)
-    } catch (e) {
-        res.status(400).send(e)
+router.post("/users/login", (req, res) => {
+  const credentials = Object.keys(req.body);
+  const requiredCredentials = ["email", "password"];
+  const isValidOperation = credentials.every((item) =>
+    requiredCredentials.includes(item)
+  );
+  if (!isValidOperation) {
+    return res.status(400).send("please send email and password");
+  }
+  User.findOne({ email: req.body.email }, (err, data) => {
+    if (!data || err) return res.send("Invalid Credentials");
+    bcrypt.compare(req.body.password, data.password).then((exists) => {
+      res.send(exists);
+    });
+  });
+});
+
+router.get("/users", async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.send(users);
+  } catch (e) {
+    res.status(500).send();
+  }
+});
+
+router.get("/users/:id", async (req, res) => {
+  const _id = req.params.id;
+
+  try {
+    const user = await User.findById(_id);
+
+    if (!user) {
+      return res.status(404).send();
     }
-})
 
-router.get('/users', async (req, res) => {
-    try {
-        const users = await User.find({})
-        res.send(users)
-    } catch (e) {
-        res.status(500).send()
-    }
-})
+    res.send(user);
+  } catch (e) {
+    res.status(500).send();
+  }
+});
 
-router.get('/users/:id', async (req, res) => {
-    const _id = req.params.id
+router.patch("/users/:id", async (req, res) => {
+  const updates = Object.keys(req.body);
+  const allowedUpdates = ["name", "email", "password", "age"];
+  const isValidOperation = updates.every((update) =>
+    allowedUpdates.includes(update)
+  );
 
-    try {
-        const user = await User.findById(_id)
+  if (!isValidOperation) {
+    return res.status(400).send({ error: "Invalid updates!" });
+  }
 
-        if (!user) {
-            return res.status(404).send()
-        }
+  try {
+    const user = await User.findById(req.params.id);
 
-        res.send(user)
-    } catch (e) {
-        res.status(500).send()
-    }
-})
+    updates.forEach((update) => (user[update] = req.body[update]));
+    await user.save();
 
-router.patch('/users/:id', async (req, res) => {
-    const updates = Object.keys(req.body)
-    const allowedUpdates = ['name', 'email', 'password', 'age']
-    const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
-
-    if (!isValidOperation) {
-        return res.status(400).send({ error: 'Invalid updates!' })
+    if (!user) {
+      return res.status(404).send();
     }
 
-    try {
-        const user = await User.findById(req.params.id)
+    res.send(user);
+  } catch (e) {
+    res.status(400).send(e);
+  }
+});
 
-        updates.forEach((update) => user[update] = req.body[update])
-        await user.save()
+router.delete("/users/:id", async (req, res) => {
+  try {
+    const user = await User.findByIdAndDelete(req.params.id);
 
-        if (!user) {
-            return res.status(404).send()
-        }
-
-        res.send(user)
-    } catch (e) {
-        res.status(400).send(e)
+    if (!user) {
+      return res.status(404).send();
     }
-})
 
-router.delete('/users/:id', async (req, res) => {
-    try {
-        const user = await User.findByIdAndDelete(req.params.id)
+    res.send(user);
+  } catch (e) {
+    res.status(500).send();
+  }
+});
 
-        if (!user) {
-            return res.status(404).send()
-        }
-
-        res.send(user)
-    } catch (e) {
-        res.status(500).send()
-    }
-})
-
-module.exports = router
+module.exports = router;
