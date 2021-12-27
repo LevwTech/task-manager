a157d383ac9ae5261bac9359b181aae8037abd4f
levvw <abdelraahmanmostafa@gmail.com>
2021-12-27 14:29:26 +0200

    removed unnecessary stuff

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ const express = require("express");
 require("./db/mongoose");
 const userRouter = require("./routers/user");
 const taskRouter = require("./routers/task");
-const productRouter = require("./routers/product"); // remove temp
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -10,7 +9,6 @@ const port = process.env.PORT || 3000;
 app.use(express.json());
 app.use(userRouter);
 app.use(taskRouter);
-app.use(productRouter); // remove temp
 
 app.listen(port, () => {
   console.log("Server is up on port " + port);
